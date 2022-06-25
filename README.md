f4d174baf0d5fb2ebe437dc53be03a44d9e24fd9
wagiminator <wagiminator@web.de>
2022-06-25 16:10:06 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,148 @@
-# ATtiny412-VoiceChanger
-Change the Pitch of your Voice
+# Voice Changer based on ATtiny412
+The ATtiny412 VoiceChanger is a simple device for changing the pitch of speech and other audio signals. An electret microphone is installed for audio input, and a small loudspeaker can be connected directly to the board for output.
+
+- Design Files (EasyEDA): https://easyeda.com/wagiminator/
+
+![pic1.jpg](https://raw.githubusercontent.com/wagiminator/ATtiny412-VoiceChanger/main/documentation/VoiceChanger_pic1.jpg)
+
+# Hardware
+The schematic is shown below:
+
+![wiring.png](https://raw.githubusercontent.com/wagiminator/ATtiny412-VoiceChanger/main/documentation/VoiceChanger_wiring.png)
+
+The inexpensive LMV358 rail-to-rail dual OpAmp is used to amplify the audio signals. The first OpAmp amplifies the weak signal from the electret microphone. The amplification factor is determined by resistors R2 and R3 (Gain = R3 / R2 = 47). The voltage divider formed by R4 and R5 at the positive input of the OpAmp defines the virtual ground and lets the audio signal oscillate around half the supply voltage. The output of the first OpAmp is connected to the ATtiny's analog-to-digital converter (ADC), which samples the amplified audio signal. This signal is modified by the ATtiny and output via its digital-to-analog converter (DAC). Since the DAC cannot drive a load directly, the signal is amplified by the LMV358's second OpAmp, which is configured as a voltage follower/buffer. The output of this OpAmp drives the speaker through a decoupling capacitor (C2).
+
+![hardware.png](https://raw.githubusercontent.com/wagiminator/ATtiny412-VoiceChanger/main/documentation/VoiceChanger_hardware.png)
+
+# Software
+## Implementation
+The amplified input signal is sampled by the ATtiny's ADC. For this purpose, the ADC is operated in free-running mode with a resolution of 8 bits and an ADC clock frequency of 500kHz. This results in a sampling rate of 500kHz/13 = 38kHz. After each conversion, an interrupt is triggered, in the service routine of which the sampled value is pushed into a 192-byte ring buffer.
+
+```c
+// ===================================================================================
+// Ring Buffer Implementation
+// ===================================================================================
+
+#define BUF_LEN       192                       // buffer length
+uint8_t BUF_buffer[BUF_LEN];                    // this is the buffer
+uint8_t BUF_head, BUF_tail;                     // head and tail pointer
+
+// Push byte to ring buffer
+void BUF_push(uint8_t data) {
+  if(++BUF_head >= BUF_LEN) BUF_head = 0;       // increase head pointer
+  BUF_buffer[BUF_head] = data;                  // push data byte to buffer
+}
+
+// Pop data byte from buffer
+uint8_t BUF_pop(void) {
+  if(++BUF_tail >= BUF_LEN) BUF_tail = 0;       // increase tail pointer
+  return BUF_buffer[BUF_tail];                  // pop data byte from buffer
+}
+
+// ===================================================================================
+// ADC Implementation for Audio Input
+// ===================================================================================
+
+// Init analog to digital converter (ADC)
+void ADC_init(void) {
+  pinDisable(PIN_ADC);                          // disable digital input buffer
+  ADC0.CTRLA   = ADC_RESSEL_8BIT_gc             // 8-bit resolution
+               | ADC_FREERUN_bm                 // free running mode
+               | ADC_ENABLE_bm;                 // enable ADC
+  ADC0.CTRLC   = ADC_SAMPCAP_bm                 // select sample capacitance
+               | ADC_REFSEL_VDDREF_gc           // set Vdd as reference
+               | ADC_PRESC_DIV8_gc;             // set prescaler -> 500kHz ADC clock
+  ADC0.MUXPOS  = PIN_ADC;                       // set the input pin
+  ADC0.INTCTRL = ADC_RESRDY_bm;                 // enable result ready interrupt
+  ADC0.COMMAND = ADC_STCONV_bm;                 // start first conversion
+}
+
+// ADC result ready interrupt service routine
+ISR(ADC0_RESRDY_vect) {
+  BUF_push(ADC0.RESL);                          // push result to buffer
+}
+```
+
+The timer/counter B (TCB) is set up in such a way that it periodically triggers another interrupt, in the service routine of which a value is pulled from the ring buffer and written to the data register of the DAC. The frequency of these interrupts can be changed using the keys. If the frequency of the timer interrupts is equal to the ADC sampling frequency (38kHz), the output signal also corresponds to the input signal. If the frequency of the timer interrupt is higher than the sampling frequency of the ADC, the pitch of the audio signal will be increased and vice versa.
+
+```c
+// ===================================================================================
+// DAC Implementation for Audio Output
+// ===================================================================================
+
+// Setup the digital to analog converter (DAC)
+void DAC_init(void) {
+  pinDisable(PIN_DAC);                          // disable digital input buffer
+  VREF_CTRLA |= VREF_DAC0REFSEL_4V34_gc;        // set DAC reference to 4.3V
+  DAC0.CTRLA  = DAC_ENABLE_bm                   // enable DAC
+              | DAC_OUTEN_bm;                   // enable output buffer
+}
+
+// Set DAC value
+void DAC_set(uint8_t value) {
+  DAC0.DATA = value;                            // set DAC
+}
+
+// ===================================================================================
+// Periodic Interrupt Implementation using TCB for Audio Output
+// ===================================================================================
+
+// Init timer/counter B (TCB)
+void TCB_init(void) {
+  TCB0.CTRLA   = TCB_CLKSEL_CLKDIV1_gc          // set prescaler
+               | TCB_ENABLE_bm;                 // enable timer
+  TCB0.CTRLB   = TCB_CNTMODE_INT_gc;            // set timer to periodic interrupt mode
+  TCB0.CCMP    = PITCH_START;                   // set TOP value
+  TCB0.INTCTRL = TCB_CAPT_bm;                   // enable interrupt
+}
+
+// Set pitch value
+void TCB_setPitch(uint8_t value) {
+  TCB0.CCMP = value;                            // set pitch value
+  TCB0.CNT  = 0;                                // reset counter
+}
+
+// Timer interrupt service routine
+ISR(TCB0_INT_vect) {
+  TCB0.INTFLAGS = TCB_CAPT_bm;                  // clear interrupt flag
+  DAC_set(BUF_pop());                           // set DAC value from buffer
+}
+```
+
+## Compiling and Uploading the Firmware
+### If using the Arduino IDE
+- Open your Arduino IDE.
+- Make sure you have installed [megaTinyCore](https://github.com/SpenceKonde/megaTinyCore).
+- Go to **Tools -> Board -> megaTinyCore** and select **ATtiny412/402/212/202**.
+- Go to **Tools** and choose the following board options:
+  - **Chip:**           ATtiny412
+  - **Clock:**          4 MHz internal
+  - Leave the rest at the default settings.
+- Connect your programmer to your PC and to the UPDI header on the board.
+- Go to **Tools -> Programmer** and select your UPDI programmer.
+- Go to **Tools -> Burn Bootloader** to burn the fuses.
+- Open the sketch and click **Upload**.
+
+###If using the makefile (Linux/Mac)
+- Connect your [programmer](https://github.com/wagiminator/AVR-Programmer) (jtag2updi or SerialUPDI) to your PC and to the UPDI header on the board.
+- Download [AVR 8-bit Toolchain](https://www.microchip.com/mplab/avr-support/avr-and-arm-toolchains-c-compilers) and extract the sub-folders (avr, bin, include, ...) to /software/tools/avr-gcc. To do this, you have to register for free with Microchip on the download site.
+- Open the makefile and set the programmer and port (default is serialupdi on /dev/ttyUSB0).
+- Open a terminal.
+- Navigate to the folder with the makefile and the sketch.
+- Run `make install` to compile, burn the fuses and upload the firmware.
+
+# Operating Instructions
+1. Connect a 5V power supply to the respective header on the board. Pay attention to the correct polarity!
+2. Connect a little speaker (max 3W/4Ω) to the respective header.
+3. Turn on the switch and speak into the microphone.
+4. Use the buttons to modify the pitch.
+
+# References, Links and Notes
+1. [ATtiny412 Datasheet](https://ww1.microchip.com/downloads/aemDocuments/documents/MCU08/ProductDocuments/DataSheets/ATtiny212-214-412-414-416-DataSheet-DS40002287A.pdf)
+2. [LMV358 Datasheet](https://datasheet.lcsc.com/szlcsc/Texas-Instruments-TI-LMV358IDR_C63813.pdf)
+
+# License
+![license.png](https://i.creativecommons.org/l/by-sa/3.0/88x31.png)
+
+This work is licensed under Creative Commons Attribution-ShareAlike 3.0 Unported License. 
+(http://creativecommons.org/licenses/by-sa/3.0/)
---
+++ software/VoiceChanger.ino
@@ -0,0 +1,221 @@
+// ===================================================================================
+// Project:   Voice Changer
+// Version:   v1.0
+// Year:      2022
+// Author:    Stefan Wagner
+// Github:    https://github.com/wagiminator
+// EasyEDA:   https://easyeda.com/wagiminator
+// License:   http://creativecommons.org/licenses/by-sa/3.0/
+// ===================================================================================
+//
+// Description:
+// ------------
+// The amplified signal from the microphone is continuously sampled by the ADC and
+// the corresponding values are written into a ring buffer. These values are in turn
+// read in at variable speed and sent to the amplifier via the DAC. The voice is
+// changed by the different sampling rates of the ADC and DAC.
+//
+// Wiring:
+// -------
+//                         +-\/-+
+//                   Vdd  1|°   |8  GND
+// AUDIO OUT --- TXD PA6  2|    |7  PA3 AIN3 -------- SLCT BUTTON
+//  AUDIO IN --- RXD PA7  3|    |6  PA0 AIN0 UPDI --- UPDI
+// UP BUTTON --- SDA PA1  4|    |5  PA2 AIN2 SCL ---- DOWN BUTTON
+//                         +----+
+//
+// Compilation Settings:
+// ---------------------
+// Core:    megaTinyCore (https://github.com/SpenceKonde/megaTinyCore)
+// Board:   ATtiny412/402/212/202
+// Chip:    ATtiny412
+// Clock:   4 MHz internal
+//
+// Leave the rest on default settings. Don't forget to "Burn bootloader"! Compile
+// and upload the code.
+//
+// No Arduino core functions or libraries are used. To compile and upload without
+// Arduino IDE download AVR 8-bit toolchain at:
+// https://www.microchip.com/mplab/avr-support/avr-and-arm-toolchains-c-compilers
+// and extract to tools/avr-gcc. Use the makefile to compile and upload.
+//
+// Fuse Settings: 0:0x00 1:0x00 2:0x01 4:0x00 5:0xC5 6:0x04 7:0x00 8:0x00
+//
+// Operating Instructions:
+// -----------------------
+// Connect a 5V power supply and a speaker (max 3W/4Ω). Turn on the device and
+// speak into the microphone. Use the buttons to change the voice.
+
+// ===================================================================================
+// Libraries, Definitions and Macros
+// ===================================================================================
+
+// Libraries
+#include <avr/io.h>               // for GPIO
+#include <avr/interrupt.h>        // for interrupts
+#include <util/delay.h>           // for delays
+
+// Pin definitions
+#define PIN_UP        PA1         // pin connected to UP button
+#define PIN_DOWN      PA2         // pin connected to DOWN button
+#define PIN_SLCT      PA3         // pin connected to SELECT button
+#define PIN_DAC       PA6         // audio output pin
+#define PIN_ADC       PA7         // audio input pin
+
+// Audio pitch values
+#define PITCH_START   103         // start value for audio pitch
+#define PITCH_MIN     63          // minimum value for audio pitch
+#define PITCH_MAX     183         // maximum value for audio pitch
+
+// Pin manipulation macros
+enum {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7};      // enumerate pin designators
+#define pinInput(x)       VPORTA.DIR &= ~(1<<(x))   // set pin to INPUT
+#define pinOutput(x)      VPORTA.DIR |=  (1<<(x))   // set pin to OUTPUT
+#define pinLow(x)         VPORTA.OUT &= ~(1<<(x))   // set pin to LOW
+#define pinHigh(x)        VPORTA.OUT |=  (1<<(x))   // set pin to HIGH
+#define pinToggle(x)      VPORTA.IN  |=  (1<<(x))   // TOGGLE pin
+#define pinRead(x)        (VPORTA.IN &   (1<<(x)))  // READ pin
+#define pinPullup(x)      (&PORTA.PIN0CTRL)[x] |= PORT_PULLUPEN_bm  // enable pullup
+#define pinDisable(x)     (&PORTA.PIN0CTRL)[x] |= PORT_ISC_INPUT_DISABLE_gc     
+
+// ===================================================================================
+// Ring Buffer Implementation
+// ===================================================================================
+
+#define BUF_LEN       192                       // buffer length
+uint8_t BUF_buffer[BUF_LEN];                    // this is the buffer
+uint8_t BUF_head, BUF_tail;                     // head and tail pointer
+
+// Push byte to ring buffer
+void BUF_push(uint8_t data) {
+  if(++BUF_head >= BUF_LEN) BUF_head = 0;       // increase head pointer
+  BUF_buffer[BUF_head] = data;                  // push data byte to buffer
+}
+
+// Pop data byte from buffer
+uint8_t BUF_pop(void) {
+  if(++BUF_tail >= BUF_LEN) BUF_tail = 0;       // increase tail pointer
+  return BUF_buffer[BUF_tail];                  // pop data byte from buffer
+}
+
+// ===================================================================================
+// ADC Implementation for Audio Input
+// ===================================================================================
+
+// Init analog to digital converter (ADC)
+void ADC_init(void) {
+  pinDisable(PIN_ADC);                          // disable digital input buffer
+  ADC0.CTRLA   = ADC_RESSEL_8BIT_gc             // 8-bit resolution
+               | ADC_FREERUN_bm                 // free running mode
+               | ADC_ENABLE_bm;                 // enable ADC
+  ADC0.CTRLC   = ADC_SAMPCAP_bm                 // select sample capacitance
+               | ADC_REFSEL_VDDREF_gc           // set Vdd as reference
+               | ADC_PRESC_DIV8_gc;             // set prescaler -> 500kHz ADC clock
+  ADC0.MUXPOS  = PIN_ADC;                       // set the input pin
+  ADC0.INTCTRL = ADC_RESRDY_bm;                 // enable result ready interrupt
+  ADC0.COMMAND = ADC_STCONV_bm;                 // start first conversion
+}
+
+// ADC result ready interrupt service routine
+ISR(ADC0_RESRDY_vect) {
+  BUF_push(ADC0.RESL);                          // push result to buffer
+}
+
+// ===================================================================================
+// DAC Implementation for Audio Output
+// ===================================================================================
+
+// Setup the digital to analog converter (DAC)
+void DAC_init(void) {
+  pinDisable(PIN_DAC);                          // disable digital input buffer
+  VREF_CTRLA |= VREF_DAC0REFSEL_4V34_gc;        // set DAC reference to 4.3V
+  DAC0.CTRLA  = DAC_ENABLE_bm                   // enable DAC
+              | DAC_OUTEN_bm;                   // enable output buffer
+}
+
+// Set DAC value
+void DAC_set(uint8_t value) {
+  DAC0.DATA = value;                            // set DAC
+}
+
+// ===================================================================================
+// Periodic Interrupt Implementation using TCB for Audio Output
+// ===================================================================================
+
+// Init timer/counter B (TCB)
+void TCB_init(void) {
+  TCB0.CTRLA   = TCB_CLKSEL_CLKDIV1_gc          // set prescaler
+               | TCB_ENABLE_bm;                 // enable timer
+  TCB0.CTRLB   = TCB_CNTMODE_INT_gc;            // set timer to periodic interrupt mode
+  TCB0.CCMP    = PITCH_START;                   // set TOP value
+  TCB0.INTCTRL = TCB_CAPT_bm;                   // enable interrupt
+}
+
+// Set pitch value
+void TCB_setPitch(uint8_t value) {
+  TCB0.CCMP = value;                            // set pitch value
+  TCB0.CNT  = 0;                                // reset counter
+}
+
+// Timer interrupt service routine
+ISR(TCB0_INT_vect) {
+  TCB0.INTFLAGS = TCB_CAPT_bm;                  // clear interrupt flag
+  DAC_set(BUF_pop());                           // set DAC value from buffer
+}
+
+// ===================================================================================
+// Button Implementation
+// ===================================================================================
+
+// Button variables
+uint8_t butlast_up = 0;                         // remember last button state
+uint8_t butlast_down = 0;                       // remember last button state
+uint8_t butlast_slct = 0;                       // remember last button state
+
+// Init buttons
+void BUT_init(void) {
+  pinPullup(PIN_UP);                            // enable internal pullup resistor
+  pinPullup(PIN_DOWN);                          // enable internal pullup resistor
+  pinPullup(PIN_SLCT);                          // enable internal pullup resistor
+}
+
+// ===================================================================================
+// Main Function
+// ===================================================================================
+
+int main(void) {
+  // Setup MCU
+  _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, 3);       // set clock frequency to 4 MHz
+
+  // Setup modules
+  BUT_init();                                   // init buttons
+  DAC_init();                                   // init DAC
+  ADC_init();                                   // init ADC
+  TCB_init();                                   // Init TCB
+  sei();                                        // enable interrupts
+  uint8_t pitch = PITCH_START;                  // set initial pitch value
+
+  // Loop
+  while(1) {
+    // Check buttons
+    uint8_t butread = pinRead(PIN_UP);          // check UP button
+    if(butread != butlast_up) {
+      if(!butread && (pitch >= PITCH_MIN)) {
+        pitch -= 20;
+        TCB_setPitch(pitch);
+      }
+      butlast_up = butread;
+      _delay_ms(10);
+    }
+
+    butread = pinRead(PIN_DOWN);                // check DOWN button
+    if(butread != butlast_down) {
+      if(!butread && (pitch <= PITCH_MAX)) {
+        pitch += 20;
+        TCB_setPitch(pitch);
+      }
+      butlast_down = butread;
+      _delay_ms(10);
+    }   
+  }
+}
